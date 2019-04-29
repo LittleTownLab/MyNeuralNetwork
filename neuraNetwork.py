3de030c5498f7c38232b4cec779b6f99909278aa
LittleTownLab <ydcstudio2016@gmail.com>
2019-04-29 10:57:47 +0800

    add new file

---
+++ neuraNetwork.py
@@ -0,0 +1,22 @@
+class neuralNetwork
+    def __init__(self, inputnode, hidennode, outputnodes, learningrate):
+        self.inodes = inputnode
+        self.hnodes = hidennode
+        self.onodes = outputnodes
+
+        self.lr = learningrate
+        pass
+
+    def train():
+        pass
+        
+    def query():
+        pass
+
+input_nodes = 3
+hidden_nodes = 3
+output_node = 3
+
+learning_rate = 0.3
+
+n = neuralNetwork(input_nodes, hidden_nodes, output_node)
